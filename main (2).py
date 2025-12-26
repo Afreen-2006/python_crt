62e49240ffe2ae319c863594597180d85618497d
Afreen-2006 <dafreen42@gmail.com>
2025-12-26 14:01:28 +0530

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,13 @@
+'''square hallow pattern'''
+
+x=int(input())
+for i in range(x):
+    for j in range(x):
+        print("",end=" ")
+        if i==0 or j==0 or i==x-1 or j==x-1:
+             print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+  
+  
