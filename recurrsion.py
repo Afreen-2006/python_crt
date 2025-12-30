5a3bfe5265f6709052e9a7560d60379908c58d69
Afreen-2006 <dafreen42@gmail.com>
2025-12-30 15:10:33 +0530

    Add files via upload

---
+++ recurrsion.py
@@ -0,0 +1,14 @@
+'''recurrsion'''
+def kk(n):
+    if n==5:
+        return
+    print((n))
+    kk(n+1)
+kk(1)
+ '''reverse number printing'''
+def kk(n):
+    if n==5:
+        return
+    kk(n+1)
+    print((n))
+kk(1)
